8cea6c3a8678ba7c5403bc57402d0e865e74d724
CHEN-BD33 <chenben10.cbd@gmail.com>
2024-10-27 12:07:02 +0200

    Update the function onBallClick(el)   to change ball color on click

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
 
 
 function onBallClick(el) {
-    const newSize = +el.textContent + 50
+    const newSize = +el.textContent + getRandomInt(20, 60)
     const size = newSize > 400 ? 100 : newSize
     el.style.width = el.style.height = (el.textContent = size) + 'px'
+    el.style.backgroundColor = getRandomColor()
 }
