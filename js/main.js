92c9a942e45fc5829ceae9cc4f6e923e369850a4
CHEN-BD33 <chenben10.cbd@gmail.com>
2024-10-27 12:32:20 +0200

    Add individual max diameter limits for each ball

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 'use strict'
 
 
-function onBallClick(el) {
+function onBallClick(el, maxDiameter) {
     const newSize = +el.textContent + getRandomInt(20, 60)
-    const size = newSize > 400 ? 100 : newSize
+    const size = newSize > maxDiameter ? 100 : newSize
     el.style.width = el.style.height = (el.textContent = size) + 'px'
     el.style.backgroundColor = getRandomColor()
 }
